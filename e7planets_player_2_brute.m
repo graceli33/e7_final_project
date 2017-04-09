7e5fb58971e6df6b0745e7baebf28bd35df42792
bobfishtincar <bhavishdinakar@berkeley.edu>
2017-04-08 19:20:20 -0700

    brute force works, but maxes out at 9 turns

--- e7planets_player_2_brute.m
+++ e7planets_player_2_brute.m
@@ -1,12 +1,12 @@
 function [direction] = e7planets_player_2_brute(map)
 
-    x_loc = map.player.location(end, 1);
-    y_loc = map.player.location(end, 2);
+    r_loc = map.player.location(end, 1);
+    c_loc = map.player.location(end, 2);
     num_turns = map.remaining_turns;
     
     grid = map.grid;
     
-    [y_grid, x_grid] = size(grid);
+    [rows, cols] = size(grid);
     
     for i=1:length(map.scraps)
         x_scrap = map.scraps(i).location(1);
@@ -14,17 +14,51 @@ function [direction] = e7planets_player_2_brute(map)
         grid(x_scrap, y_scrap) = map.scraps(i).value;
     end
     
-    grid
-
-    direction = 'U';
+    [value, path] = brute_force(r_loc, c_loc, num_turns, grid, rows, cols);
+    
+    direction = path(1);
+    
     return;
 
 end
 
-function [value, path] = brute_force(x_loc, y_loc, num_turns, grid, x_grid, y_grid)
+function [value, path] = brute_force(r_loc, c_loc, num_turns, grid, rows, cols)
         
-    if num_turns == 1
-        up_val = grid(x_loc, mod(y_loc-1, y_grid));
+    if num_turns == 0
+        
+        value = grid(r_loc, c_loc);
+        path = '';
+        
+        return;
+        
+    end
+        
+    [u_val, path] = brute_force(mod(r_loc-2, rows)+1, c_loc, num_turns-1, grid, rows, cols);
+    [d_val, path] = brute_force(mod(r_loc, rows)+1, c_loc, num_turns-1, grid, rows, cols);
+    [l_val, path] = brute_force(r_loc, mod(c_loc-2, cols)+1, num_turns-1, grid, rows, cols);
+    [r_val, path] = brute_force(r_loc, mod(c_loc, cols)+1, num_turns-1, grid, rows, cols);
+        
+    max_val = max([u_val, d_val, l_val, r_val]);
+    
+    if max_val == u_val
+        
+        value = grid(r_loc, c_loc) + u_val;
+        path = ['U', path];
+        
+    elseif max_val == d_val
+        
+        value = grid(r_loc, c_loc) + d_val;
+        path = ['D', path];
+        
+    elseif max_val == l_val
+        
+        value = grid(r_loc, c_loc) + l_val;
+        path = ['L', path];
+        
+    elseif max_val == r_val
+        
+        value = grid(r_loc, c_loc) + r_val;
+        path = ['R', path];
         
     end
 
